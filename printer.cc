a2c3de5bda9a4d391eb6001775319b5abd157e8e
George Chen <ggchen2k2@gmail.com>
2023-11-30 17:05:47 -0500

    printer

--- printer.cc
+++ printer.cc
@@ -1,44 +1,134 @@
+#include <iostream>
 #include "printer.h"
+using namespace std;
 
-Printer::print(Kind kind, char state) {
-    if(!buffer[kind].empty) flush_buffer();
-    buffer[kind].state = state;
+void Printer::print(Kind kind, char state) {
+    unsigned int idx = getColumn(kind);
+    if(buffer[idx].defined) flushBuffer();
+    buffer[idx].state = state;
+    buffer[idx].defined = true;
 }
 
-Printer::print(Kind kind, char state, unsigned int value1) {
-    if(!buffer[kind].empty) flush_buffer();
-    buffer[kind].state = state;
-    buffer[kind].value1 = value1;
+void Printer::print(Kind kind, char state, unsigned int value1) {
+    unsigned int idx = getColumn(kind);
+    if(buffer[idx].defined) flushBuffer();
+    buffer[idx].state = state;
+    buffer[idx].value1 = value1;
+    buffer[idx].defined = true;
+    buffer[idx].def1 = true;
 }
 
-Printer::print(Kind kind, char state, unsigned int value1, unsigned int value2) {
-    if(!buffer[kind].empty) flush_buffer();
-    buffer[kind].state = state;
-    buffer[kind].value1 = value1;
-    buffer[kind].value2 = value2;
+void Printer::print(Kind kind, char state, unsigned int value1, unsigned int value2) {
+    unsigned int idx = getColumn(kind);
+    if(buffer[idx].defined) flushBuffer();
+    buffer[idx].state = state;
+    buffer[idx].value1 = value1;
+    buffer[idx].value2 = value2;
+    buffer[idx].defined = true;
+    buffer[idx].def1 = true;
+    buffer[idx].def2 = true;
 }
 
-Printer::print(Kind kind, unsigned int lid, char state) {
-    if(!buffer[kind].empty) flush_buffer();
-    buffer[kind].lid = lid;
-    buffer[kind].state = state;
+void Printer::print(Kind kind, unsigned int lid, char state) {
+    unsigned int idx = getColumn(kind, lid);
+    if(buffer[idx].defined) flushBuffer();
+    buffer[idx].state = state;
+    buffer[idx].defined = true;
 }
 
-Printer::print(Kind kind, unsigned int lid, char state, unsigned int value1) {
-    if(!buffer[kind].empty) flush_buffer();
-    buffer[kind].lid = lid;
-    buffer[kind].state = state;
-    buffer[kind].value1 = value1;
+void Printer::print(Kind kind, unsigned int lid, char state, unsigned int value1) {
+    unsigned int idx = getColumn(kind, lid);
+    if(buffer[idx].defined) flushBuffer();
+    buffer[idx].state = state;
+    buffer[idx].value1 = value1;
+    buffer[idx].defined = true;
+    buffer[idx].def1 = true;
 }
 
-Printer::print(Kind kind, unsigned int lid, char state, unsigned int value1, unsigned int value2) {
-    if(!buffer[kind].empty) flush_buffer();
-    buffer[kind].lid = lid;
-    buffer[kind].state = state;
-    buffer[kind].value1 = value1;
-    buffer[kind].value2 = value2;
+void Printer::print(Kind kind, unsigned int lid, char state, unsigned int value1, unsigned int value2) {
+    unsigned int idx = getColumn(kind);
+    if(buffer[idx].defined) flushBuffer();
+    buffer[idx].state = state;
+    buffer[idx].value1 = value1;
+    buffer[idx].value2 = value2;
+    buffer[idx].defined = true;
+    buffer[idx].def1 = true;
+    buffer[idx].def2 = true;
 }
 
-Printer::flush_buffer() {
-    
+Printer::Printer(unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers)
+  : numStudents(numStudents), numVendingMachines(numVendingMachines), numCouriers(numCouriers) {
+    totalCols = singletons + numStudents + numVendingMachines + numCouriers;
+    buffer = new PrintState[totalCols];
+    // print header
+    for (unsigned int i=0;i<singletons;i++) { cout << classNames[i] << "\t"; }
+    // print students
+    for (unsigned int i=0;i<numStudents;i++) { cout << classNames[i] << i << "\t"; }
+    // print machines
+    for (unsigned int i=0;i<numStudents;i++) { cout << classNames[i] << i << "\t"; }
+    // print couriers
+    for (unsigned int i=0;i<numStudents;i++) {
+        cout << classNames[i] << i << (i+1 == numStudents? "\n":"\t");
+    }
+
+    for (unsigned int i=0;i<totalCols;i++) {
+        cout << "*******" << (i+1 == totalCols? "\n":"\t");
+    }
+}
+
+Printer::~Printer() {
+    cout << "***********************" << endl;
+}
+
+unsigned int Printer::getColumn(Kind kind) {
+    unsigned int idx = 0;
+    switch (kind) {
+      case Parent: idx = 0; break;
+      case GroupOff: idx = 1; break;
+      case WATCardOffice: idx = 2; break;
+      case NameServer: idx = 3; break;
+      case Truck: idx = 4; break;
+      case BottlingPlant: idx = 5; break;
+      default:
+        assert(false); // invalid type
+    }
+    return idx;
+}
+
+unsigned int Printer::getColumn(Kind kind, unsigned int lid) {
+    unsigned int idx = 0;
+    switch (kind) {
+      case Student:
+        idx = singletons + lid; break;
+      case Vending:
+        idx = singletons + numStudents + lid; break;
+      case Courier:
+        idx = singletons + numStudents + numVendingMachines + lid; break;
+      default:
+        assert(false); // invalid type
+    }
+    return idx;
+}
+
+void Printer::displayState(PrintState &s, char suf) {
+    cout << s.state;
+    if (s.def1) { cout << to_string(s.value1); }
+    if (s.def2) {
+        assert(s.def1);
+        cout << "," << to_string(s.value2);
+    }
+    cout << suf;
+}
+
+void Printer::flush_buffer() {
+    unsigned int lastDefined = 0;
+    for (unsigned int i=0;i<totalCols;i++) {
+        if (buffer[i].defined) { lastDefined = i+1; }
+    }
+    for (unsigned int i=0;i<lastDefined;i++) {
+        displayState(buffer[i], (i+1 == lastDefined? '\n':'\t'));
+        buffer[i].defined = false; // clear state
+        buffer[i].def1 = false;
+        buffer[i].def2 = false;
+    }
 }
--- printer.h
+++ printer.h
@@ -2,22 +2,36 @@
 #define _PRINTER_H_
 
 _Monitor Printer {
-  //printState is a struct for storing printing information
-  struct printState {
-	char state;
-	unsigned int value1;
-	unsigned int value2;
-    unsigned int lid;
-    bool empty = true;
-  };
+	// PrintState is a struct for storing printing information
+	struct PrintState {
+		char state;
+		unsigned int value1;
+		unsigned int value2;
+		// flags for whether this state is defined or not
+		// and whether value1 or value2 is populated
+		bool defined = false, def1 = false, def2 = false;
+	};
 
-  //buffer is a dynamically allocated array of printStates for storing printing information for each voter
-  printState *buffer;
-  unsigned int numStudents, numVendingMachines, numCouriers; 
-  void flush_buffer();
+  	// buffer is a dynamically allocated array of PrintStates for storing printing information for each class
+	unsigned int numStudents, numVendingMachines, numCouriers, totalCols;
+	PrintState *buffer;
+
+	// print a full line of the buffer and empty out all states
+	void flushBuffer();
+	// print a single state with a given suffix
+	void displayState(PrintState &s, char suf = '\t');
+	
+	// helper functions to get which columns the print call should be displayed in
+	unsigned int getColumn(Kind kind);
+	unsigned int getColumn(Kind kind, unsigned int lid);
+
+	// display names of each class
+	const char* []classNames = ["Parent", "Gropoff", "WATOff", "Names", "Truck", "Plant", "Stud", "Mach", "Cour"];
+	const unsigned int singletons = 6; // number of classes that only have a single instance
   public:
 	enum Kind { Parent, Groupoff, WATCardOffice, NameServer, Truck, BottlingPlant, Student, Vending, Courier };
 	Printer( unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers );
+	~Printer();
 	void print( Kind kind, char state );
 	void print( Kind kind, char state, unsigned int value1 );
 	void print( Kind kind, char state, unsigned int value1, unsigned int value2 );
