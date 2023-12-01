168b557933b76f79c51e383f84b435e29d7e7352
Umar Yousafzai <39500143+uyousafzai54@users.noreply.github.com>
2023-12-01 13:40:58 -0500

    mock nameServer

---
+++ nameServer.cc
@@ -0,0 +1,22 @@
+#include "nameServer.h"
+#include "vendingMachine.h"
+
+
+NameServer::NameServer(Printer & prt, unsigned int numVendingMachines, unsigned int numStudents) {}
+
+void NameServer::VMregister(VendingMachine *vendingmachine) {
+
+}
+
+VendingMachine * NameServer::getMachine(unsigned int id) {
+
+}
+
+VendingMachine ** NameServer::getMachineList() {
+
+}
+
+void NameServer::void() {
+
+}
+
---
+++ nameServer.h
@@ -0,0 +1,13 @@
+ #ifndef _NAME_SERVER_H
+ #define _NAME_SERVER_H
+ 
+ _Task NameServer {
+    void main();
+   public:
+    NameServer( Printer & prt, unsigned int numVendingMachines, unsigned int numStudents );
+    void VMregister( VendingMachine * vendingmachine );
+    VendingMachine * getMachine( unsigned int id ) __attribute__(( warn_unused_result ));
+    VendingMachine ** getMachineList() __attribute__(( warn_unused_result ));
+};
+
+#endif
