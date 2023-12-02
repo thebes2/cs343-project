025ad4629328fdee72b7af987c86660e4ca0ec23
Umar Yousafzai <39500143+uyousafzai54@users.noreply.github.com>
2023-12-02 17:19:30 -0500

    move main to soda

--- soda.cc
+++ soda.cc
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
-#include "config.h"
 #include "printer.h"
-#include "bank.h"
-#include "parent.h"
+#include "watCardOffice.h"
+#include "config.h"
 #include "groupoff.h"
+#include "student.h"
+#include "vendingMachine.h"
 using namespace std;
 
 int main(int argc, char *argv[]) {
@@ -38,19 +39,27 @@ int main(int argc, char *argv[]) {
 		exit(EXIT_FAILURE);
 	}
 
-	// basic test where we will launch the parent and bank and see if they compile
-	Printer printer(config.numStudents, config.numVendingMachines, config.numCouriers);
-	Bank bank(config.numStudents);
-	Parent parent(printer, bank, config.numStudents, config.parentalDelay);
-	Groupoff groupoff(printer, config.numStudents, config.sodaCost, config.groupoffDelay);
-	// simulate all students calling groupoff
-	for (unsigned int i=0;i<config.numStudents;i++) {
-		groupoff.giftCard();
-	}
-	float a = 2.0;
-	// spin for some time...
-	for (int i=0;i<1000000000;i++) {
-		a = a*a - 2.0;
-	}
-	return 0;
+    ConfigParms c;
+    Printer printer(c.numStudents, c.numVendingMachines, c.numCouriers);
+    Bank bank(c.numStudents);
+    WATCardOffice office(printer, bank, c.numCouriers);
+    Groupoff g(printer, c.numStudents, c.sodaCost, c.groupoffDelay);
+    NameServer nameServer(printer, c.numVendingMachines, c.numStudents);
+    BottlingPlant bottlingPlant(printer, nameServer, c.numVendingMachines, c.maxShippedPerFlavour, c.maxStockPerFlavour, c.timeBetweenShipments);
+
+    VendingMachine *machines[c.numVendingMachines];
+    Student *students[c.numStudents];
+    for(unsigned int i=0; i<c.numVendingMachines; i++) {
+        machines[i] = new VendingMachine(printer, nameServer, i, c.sodaCost);
+    }
+    for(unsigned int i=0; i<c.numStudents; i++) {
+        students[i] = new Student(printer, nameServer, office, g, i, c.maxPurchases);
+    }
+
+    for(unsigned int i=0; i<c.numVendingMachines; i++) {
+        delete machines[i];
+    }
+    for(unsigned int i=0; i<c.numStudents; i++) {
+        delete students[i];
+    }
 }
