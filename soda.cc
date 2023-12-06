f6fd164813be59cac9e07c53fbff5fdf53f13400
Umar Yousafzai <39500143+uyousafzai54@users.noreply.github.com>
2023-12-05 22:58:15 -0500

    spurious wakeups

--- soda.cc
+++ soda.cc
@@ -12,23 +12,23 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 	string configFile = "soda.config";
-	unsigned int seed = 0, processors = 1;
+	unsigned int seed = 0, processors = 1; // seed is 0 if we are using default seed
 	ConfigParms c;
 	struct cmd_error{};
 	try {
 		switch(argc) {
 			case 4:
-				if (strcmp(argv[3], "d") != 0) {
+				if (strcmp(argv[3], "d") != 0) { // parse number of processors
                     processors = convert(argv[3]);
                     if (processors <= 0) throw cmd_error{};
                 }
 			case 3:
-				if (strcmp(argv[2], "d") != 0) {
+				if (strcmp(argv[2], "d") != 0) { // parse random seed
 					seed = convert(argv[2]);
 					if(seed<=0) throw cmd_error{};
 				}
 			case 2:
-				if (strcmp(argv[1], "d") != 0) {
+				if (strcmp(argv[1], "d") != 0) { // config file
 					configFile = argv[1];
 				}
 			case 1: break;
@@ -41,10 +41,11 @@ int main(int argc, char *argv[]) {
 		exit(EXIT_FAILURE);
 	}
 
-	if (seed != 0) { set_seed(seed); }
+	if (seed != 0) { set_seed(seed); } // if seed is provided, use it
 
 	uProcessor p[processors - 1]; // number of kernel threads
 	{
+		// launch tasks
 		Printer printer(c.numStudents, c.numVendingMachines, c.numCouriers);
 		Bank bank(c.numStudents);
 		Parent parent(printer, bank, c.numStudents, c.parentalDelay);
@@ -52,7 +53,7 @@ int main(int argc, char *argv[]) {
 		Groupoff g(printer, c.numStudents, c.sodaCost, c.groupoffDelay);
 		NameServer nameServer(printer, c.numVendingMachines, c.numStudents);
 		VendingMachine *machines[c.numVendingMachines];
-		for(unsigned int i=0; i<c.numVendingMachines; i++) {
+		for(unsigned int i=0; i<c.numVendingMachines; i++) { // launch all vending machines
 			machines[i] = new VendingMachine(printer, nameServer, i, c.sodaCost);
 		}
 		{
@@ -62,12 +63,12 @@ int main(int argc, char *argv[]) {
 			for(unsigned int i=0; i<c.numStudents; i++) {
 				students[i] = new Student(printer, nameServer, office, g, i, c.maxPurchases);
 			}
-			for(unsigned int i=0; i<c.numStudents; i++) {
+			for(unsigned int i=0; i<c.numStudents; i++) { // wait for all students to complete
 				delete students[i];
 			}
-		}
-		for(unsigned int i=0; i<c.numVendingMachines; i++) {
+		} // we use a nested block here to force bottlingPlant to shut down after student shuts down
+		for(unsigned int i=0; i<c.numVendingMachines; i++) { // wait for vending machines to shut down
 			delete machines[i];
-		}
+		} // wait for all other tasks to complete
 	}
 }
